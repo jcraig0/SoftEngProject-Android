ff0c9a15c34da130240b6743f279d96cdb12af6f
jcraig0 <jovon.craig@maine.edu>
2018-03-29 15:55:04 -0400

    Added to employee screen

--- app/src/main/java/com/example/thorm/test4/Test4_2.java
+++ app/src/main/java/com/example/thorm/test4/Test4_2.java
@@ -1,17 +1,18 @@
 package com.example.thorm.test4;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ListView;
-import android.widget.PopupMenu;
+import android.widget.SimpleAdapter;
 import android.widget.Spinner;
-import android.widget.TextView;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Test4_2 extends AppCompatActivity {
 
@@ -21,60 +22,56 @@ public class Test4_2 extends AppCompatActivity {
         setContentView(R.layout.activity_test4_2);
         setTitle("Employee List");
 
+        String[][] myArray = {{"Aaron", "ID: 1"}, {"Justin", "ID: 2"}, {"Tim", "ID: 3"}, {"Timothy", "ID: 4"}, {"Aaron", "ID: 5"}, {"Jovon", "ID: 6"}};
+        ArrayList<HashMap<String,String>> hash = new ArrayList<>();
+        for (String[] entry : myArray) {
+            HashMap<String,String> item = new HashMap<>();
+            item.put("line1", entry[0]);
+            item.put("line2", entry[1]);
+            hash.add(item);
+        }
+
+        SimpleAdapter adapter = new SimpleAdapter(this, hash, R.layout.linear_layout, new String[]{"line1","line2"}, new int[]{R.id.textView1, R.id.textView2});
+
+        final ListView employeeList = (ListView) findViewById(R.id.employeeLV);
+        employeeList.setAdapter(adapter);
+
+        /*
         String[] myArray = "Aaron,Justin,Tim,Timothy,Patrick,Jovon".split(",");
         final ListView employeeList = (ListView) findViewById(R.id.employeeLV);
-        final ArrayAdapter employeeAdaptor = new ArrayAdapter(this, R.layout.text_view, myArray);
-
+        final ArrayAdapter employeeAdaptor = new ArrayAdapter(this, R.layout.linear_layout, myArray);
         employeeList.setAdapter(employeeAdaptor);
+        */
+    }
 
-        final Intent toMainActivity = new Intent(this, MainActivity.class);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
 
-        employeeList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-                TextView t = (TextView) view;
-                t.setText(String.valueOf(position));
-
-            }
-        });
-
-        String[] test32 = "D,E,F,G".split(",");
-        ArrayAdapter test1 = new ArrayAdapter(this, R.layout.text_view, test32 );
-        final Spinner spin = (Spinner) findViewById(R.id.sortingSpinner);
-        spin.setAdapter(test1);
-        spin.setDropDownVerticalOffset(100);
-
-        final Button b = (Button) findViewById(R.id.testButtonB);
-
-        b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                PopupMenu menu = new PopupMenu(Test4_2.this, v);
-
-                menu.getMenu().add("Hello");
-                menu.getMenu().getItem(0).setIcon(android.R.drawable.arrow_down_float);
-
-                menu.getMenu().add("World");
-                menu.getMenu().getItem(1).setIcon(android.R.drawable.arrow_up_float);
-
-                menu.show();
-
-                menu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.order:
+                String[] orders = "Alphabetical,Alphabetical (Reverse),Unfilled First".split(",");
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("Choose Order")
+                       .setItems(orders, new DialogInterface.OnClickListener() {
                     @Override
-                    public boolean onMenuItemClick(MenuItem item) {
+                    public void onClick(DialogInterface dialogInterface, int i) {
 
-
-                        b.setText(item.getTitle());
-                        b.setBackground(item.getIcon());
-
-                        return false;
                     }
                 });
-            }
-        });
-
-
+                builder.create().show();
+                return true;
+            case R.id.signout:
+                Intent toMainActivity = new Intent(this, MainActivity.class);
+                startActivity(toMainActivity);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
+
 }
